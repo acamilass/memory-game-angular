161257dc2e9b2139a549a5baa5dc6a5d431258bd
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-12-04 18:25:22 -0300

    Criar grid e níveis de dificuldade

--- src/app/memory-game/memory-game.component.ts
+++ src/app/memory-game/memory-game.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemoryGameComponent implements OnInit {
 
+  cardsImages = [
+    'assets/natal/1.png',
+    'assets/natal/2.png',
+    'assets/natal/3.png',
+    'assets/natal/4.png',
+    'assets/natal/5.png',
+    'assets/natal/6.png',
+  ];
+
+  cardsArr: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  shuffle(newArray: Array<string>): Array<string> {
+    let currentIndex = newArray.length;
+    let temporaryValue;
+    let randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = newArray[currentIndex];
+      newArray[currentIndex] = newArray[randomIndex];
+      newArray[randomIndex] = temporaryValue;
+    }
+
+    return newArray;
+  }
+
+  selectLevel(type: string) {
+
+    if (type === 'easy') {
+      this.cardsArr = this.createNewArray(2);
+    } else if (type === 'medium') {
+      this.cardsArr = this.createNewArray(4);
+    } else if (type === 'hard') {
+      this.cardsArr = this.createNewArray(6);
+    }
+
+  }
+
+  createNewArray(lengthArray: number) {
+
+    let newArray = [];
+    const arr = this.shuffle(this.cardsImages);
+
+    for (var i = 0; i < lengthArray; i++) {
+      newArray.push(arr[i], arr[i]);
+    }
+
+    return this.shuffle(newArray);
   }
 
 }
